616fd3d0887aaf80b515d604fc3429620a328974
Pere Thinkink <thinkink@thinkink.es>
2016-05-31 05:32:37 +0200

    Create signink.js

---
+++ signink.js
@@ -0,0 +1,47 @@
+// signink
+
+
+signink = function(name, w , h) {
+        name  = name || '';
+        w  = w || 60;
+        h  = h || 60;
+
+        var colours = [
+                "#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50",
+                "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"
+            ],
+
+            nameSplit = String(name).toUpperCase().split(' '),
+            initials, charIndex, colourIndex, canvas, context, dataURI;
+
+
+        if (nameSplit.length == 1) {
+            initials = nameSplit[0] ? nameSplit[0].charAt(0):'?';
+        } else {
+            initials = nameSplit[0].charAt(0) + nameSplit[1].charAt(0);
+        }
+
+        if (window.devicePixelRatio) {
+            //size = (size * window.devicePixelRatio);
+        }
+
+        charIndex     = (initials == '?' ? 72 : initials.charCodeAt(0)) - 64;
+        colourIndex   = charIndex % 20;
+        canvas        = document.createElement('canvas');
+        canvas.width  = w;
+        canvas.height = h;
+        context       = canvas.getContext("2d");
+
+        context.fillStyle = colours[colourIndex - 1];
+        context.fillRect (0, 0, canvas.width, canvas.height);
+        context.font = Math.round(canvas.height/1.7)+"px Arial";
+        context.textAlign = "center";
+        context.fillStyle = "#FFF";
+		//name //initials
+        context.fillText(name, w / 2, h / 1.5);
+
+        dataURI = canvas.toDataURL();
+        canvas  = null;
+
+        return dataURI;
+    };
